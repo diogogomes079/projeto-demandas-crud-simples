4ede3c36f5f42c6067cfce7066de566d277192c2
Diogo Gomes Pinheiro <diogogomes079@gmail.com>
2023-04-22 11:48:46 -0300

    Add files via upload

---
+++ Constantes.py
@@ -0,0 +1,9 @@
+LISTA_STATUS = ["Encaminhado",
+                "Aceito",
+                "Em Atendimento",
+                "Em Espera",
+                "Aguardando parceiro",
+                "Aguardando resposta do usuário final",
+                "Resolvido",
+                "Fechado"]
+CABECALHO = ['Data', 'Demanda', 'Status', 'Resumo', 'Parceiro']
